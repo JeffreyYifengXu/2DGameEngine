0d119935a883e6ef1419e43cf92380f99bc19927
Jeffrey Yifeng xu <jexu2915@uni.sydney.edu.au>
2024-03-08 22:07:16 +1100

    USed regex to perform shader abstraction, created shader class

--- app/src/main/java/jade/LevelEditorScene.java
+++ app/src/main/java/jade/LevelEditorScene.java
@@ -1,35 +1,130 @@
 package jade;
 
-import java.awt.event.KeyEvent;
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL15.glGenBuffers;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import org.lwjgl.BufferUtils;
+
+import Renderer.Shader;
+
+import static org.lwjgl.opengl.ARBVertexArrayObject.glBindVertexArray;
+import static org.lwjgl.opengl.ARBVertexArrayObject.glGenVertexArrays;
 
 public class LevelEditorScene extends Scene{
-    
-    private boolean changingScene = false;
-    private float timeToChangeScene = 2.0f;
 
+    // private String vertexShaderSrc = "#version 330 core\r\n" + //
+    //             "layout (location=0) in vec3 aPos;\r\n" + //
+    //             "layout (location=1) in vec4 aColor;\r\n" + //
+    //             "\r\n" + //
+    //             "out vec4 fColor;\r\n" + //
+    //             "\r\n" + //
+    //             "void main() {\r\n" + //
+    //             "    fColor = aColor;\r\n" + //
+    //             "    gl_Position = vec4(aPos, 1.0);\r\n" + //
+    //             "}";
+
+    // private String fragmentShaderSrc = "#version 330 core\r\n" + //
+    //             "\r\n" + //
+    //             "in vec4 fColor;\r\n" + //
+    //             "\r\n" + //
+    //             "out vec4 color;\r\n" + //
+    //             "\r\n" + //
+    //             "void main() {\r\n" + //
+    //             "    color = fColor;\r\n" + //
+    //             "}";
+
+    // private int vertexID, fragmentID, shaderProgram;
+
+    private float[] vertexArray = {
+        //position                  //color
+         0.5f, -0.5f, 0.0f,         1.0f, 0.0f, 0.0f, 1.0f, //Bottom right 
+        -0.5f, 0.5f, 0.0f,          0.0f, 1.0f, 0.0f, 1.0f, //Top left     
+         0.5f, 0.5f, 0.0f,          0.0f, 0.0f, 1.0f, 1.0f, //Top right
+        -0.5f, -0.5f, 0.0f,         1.0f, 1.0f, 0.0f, 1.0f, //Bottom left
+    };
+
+    //Must be in counter-clockwise order
+    private int[] elementArray = {
+        2, 1, 0, //Top right triangle
+        0, 1, 3  // bottom left triangle
+    };
+
+    private int vaoID, vboID, eboID;
+
+    private Shader defaultShader;
+    
     public LevelEditorScene() {
-        System.out.println("Inside level editor scene.");
+    }
+
+    @Override
+    public void init() {
+        defaultShader = new Shader("app/assests/shaders/default.glsl");
+        defaultShader.compile();
+        //Generaate VAO, VBO and EBO buffer objects, and send to GPU
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        //Create a float buffer of vertices
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
+        vertexBuffer.put(vertexArray).flip();
+
+        //Create VBO upload the vertex buffer
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
+
+        //create the indices and upload
+        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
+        elementBuffer.put(elementArray).flip();
+
+        eboID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+        //Add the vertex attribute pointers
+        int positionSize = 3;
+        int colorSize = 4;
+        int floatSizeBytes = 4;
+        int vertexSizeBytes = (positionSize + colorSize) * floatSizeBytes;
+        glVertexAttribPointer(0, positionSize, GL_FLOAT, false, vertexSizeBytes, 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionSize * floatSizeBytes);
+        glEnableVertexAttribArray(1);
     }
 
     @Override
     public void update(float dt) {
+        //Bind shader program
+        defaultShader.use();
+        //bind the VAO
+        glBindVertexArray(vaoID);
+        
+        //Enable the vertex attribute pointers
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
 
-        System.out.println("" + (1.0f/dt) + "FPS");
+        glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
 
-        if (!changingScene && KeyListener.isKeyPressed(KeyEvent.VK_SPACE)) {
-            changingScene = true;
-        }
-        if (changingScene && timeToChangeScene > 0) {
-            timeToChangeScene -= dt; //Take away the time that's passed in the last frame
-            Window.get().r -= dt * 5.0f;
-            Window.get().g -= dt * 5.0f;
-            Window.get().b -= dt * 5.0f;
-            // System.out.println("Changing color!!");
-        }
-        else if (changingScene) { //change scene
-            Window.changeScene(1);
-        }
+        //unbind everything
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
 
+        glBindVertexArray(0);
+
+        defaultShader.detach();
     }
-
 }
